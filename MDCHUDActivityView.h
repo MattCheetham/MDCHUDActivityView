b1d5cccb79d08c8817e9de2018152f2277d57759
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-04-26 13:30:12 +0100

    Creates initial file

---
+++ MDCHUDActivityView.h
@@ -0,0 +1,13 @@
+//
+//  MDCHUDActivityView.h
+//  MDCHudActivityViewExample
+//
+//  Created by Matthew Cheetham on 26/04/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDCHUDActivityView : UIView
+
+@end
---
+++ MDCHUDActivityView.m
@@ -0,0 +1,31 @@
+//
+//  MDCHUDActivityView.m
+//  MDCHudActivityViewExample
+//
+//  Created by Matthew Cheetham on 26/04/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import "MDCHUDActivityView.h"
+
+@implementation MDCHUDActivityView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
